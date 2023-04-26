3b1bb759e327a9659a00add41bf0d54c99e85301
Dossal <52135837+Dossal@users.noreply.github.com>
2023-04-27 02:18:07 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -7,7 +7,7 @@ cd ./pbr
 wget --no-check-certificate -c -O CNip1.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
 
 # Download the second file
-curl -sL https://raw.githubusercontent.com/DMF2022/ROS-cnip-script/main/cnip.rsc | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' > CNip2.txt
+curl -sL https://raw.githubusercontent.com/DMF2022/ROS-cnip-script/main/cnip.rsc | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}' > CNip2.txt
 
 # Combine both files and remove duplicates
 cat CNip1.txt CNip2.txt | sort -u > CNip.txt
