b6c4097ba90d4442eef61bb0effa545c3bce891c
Dossal <52135837+Dossal@users.noreply.github.com>
2025-06-30 00:24:29 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -22,5 +22,19 @@ done
     
 } > ../CNip.rsc
 
+# Download the ipv6 file
+wget --no-check-certificate -c -O CNIPv6.rsc https://github.com/zealic/autorosvpn/blob/master/chnroutes.ipv6.rsc | grep -Eo "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))(\/[0-9]+)?" | sort -t : -n
+}
+
+{
+ echo "/ipv6 firewall address-list remove [/ipv6 firewall address-list find list=CNIPv6]"
+  echo "/ipv6 firewall address-list"
+  while read line; do
+    local fields=($(echo "$line"))
+    local address=${fields[0]}
+    echo "add address=$address disabled=no list=CNipv6"
+  done
+} > ../CNIPv6.rsc
+
 cd ..
 rm -rf ./pbr
